b986dd6025ae92aa2017ec03a792d126c97a33f5
sagarshrestha81 <sagarcrestha81@gmail.com>
2024-07-01 08:24:28 +0545

    function

---
+++ js/day-09/main.js
@@ -0,0 +1,19 @@
+// function total(x,y,...abc) {
+// console.log(x);
+// console.log(y);
+// console.log(abc);
+// }
+
+// marks=total(45,65,34,78,67,5434,"ram",[1,2,3]);
+
+// const fun=(x,y)=>x+y
+
+// console.log(fun(23,32))
+
+
+function fun(){
+    document.getElementById("message").innerHTML='<span style="color: #ff0000;">you click red</span>';
+}
+function fun2(){
+    document.getElementById("message").innerHTML='<span style="color: #00ff00;">you click green</span>';
+}
